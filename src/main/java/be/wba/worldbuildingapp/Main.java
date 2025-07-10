a732bd65b511a4eb4197d5a78937c71af3eee57c
yoniz <yonizaman@hotmail.com>
2025-07-10 14:18:13 +0200

    Added homepage

--- src/main/java/be/wba/worldbuildingapp/Main.java
+++ src/main/java/be/wba/worldbuildingapp/Main.java
@@ -1,4 +1,26 @@
 package be.wba.worldbuildingapp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file from resources/view/HomeScreen.fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/HomePage.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Set up the main window
+        primaryStage.setTitle("Worldbuilding App");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/be/wba/worldbuildingapp/controller/HomeController.java
+++ src/main/java/be/wba/worldbuildingapp/controller/HomeController.java
@@ -1,4 +1,38 @@
-package be.wba.controller;
+package be.wba.worldbuildingapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
 
 public class HomeController {
+
+    @FXML
+    private ListView<String> projectList;
+
+    @FXML
+    private void initialize() {
+        // Temporary dummy data
+        projectList.getItems().addAll("My Fantasy World", "Dawn of the Scion");
+    }
+
+    @FXML
+    private void handleNewProject() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setHeaderText("Create New Project");
+        dialog.setContentText("Project name:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> projectList.getItems().add(name));
+    }
+
+    @FXML
+    private void handleOpenProject() {
+        String selected = projectList.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            System.out.println("Opening project: " + selected);
+            // Later: load project workspace
+        }
+    }
 }
